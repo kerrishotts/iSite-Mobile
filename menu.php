e477ae62c4fae0195d936db06470e0d7e68cd439
Kerri Shotts <kerrishotts@gmail.com>
2011-05-26 17:12:26 -0500

    Changed menu to reflect demo site

--- menu.php
+++ menu.php
@@ -2,8 +2,8 @@
                     <h2>Navigation</h2>
                     <div class="navSubItemGroup" id="grpMainMenu" >
                         <a id="mnuMainHome"       title="Home"       href="javascript:loadContent('./home.php', updateMainMenu );"       class="navSubItem sel"><img src="./images/tabsfreecolor/Circle-Info.png"> Home</a>
-                        <a id="mnuMainNews"       title="News"       href="javascript:loadContent('./news.php', updateMainMenu );"       class="navSubItem"><img src="./images/tabsfreecolor/RSS-Alternative.png"> News &amp; Events</a>
-                        <a id="mnuMainMinistries" title="Ministries" href="javascript:loadContent('./ministries.php', updateMainMenu );" class="navSubItem"><img src="./images/tabsfreecolor/Heart.png"> Ministries</a>
+                        <a id="mnuMainNews"       title="News"       href="javascript:loadContent('./news.php', updateMainMenu );"       class="navSubItem"><img src="./images/tabsfreecolor/RSS-Alternative.png"> News</a>
+                        <a id="mnuMainExamples"   title="Examples"   href="javascript:loadContent('./examples.php', updateMainMenu );"   class="navSubItem"><img src="./images/tabsfreecolor/Heart.png"> Examples</a>
                         <a id="mnuMainPodcasts"   title="Podcasts"   href="javascript:loadContent('./podcasts.php', updateMainMenu );"   class="navSubItem"><img src="./images/tabsfreecolor/Music.png"> Podcasts</a>
                         <a id="mnuMainTweets"     title="Tweets"     href="javascript:loadContent('./tweets.php', updateMainMenu );"     class="navSubItem"><img src="./images/tabsfreecolor/Twitter.png"> Latest Tweets</a>
                         <a id="mnuMainFacebook"   title="Facebook"   href="javascript:loadContent('./facebook.php', updateMainMenu );"   class="navSubItem"><img src="./images/tabsfreecolor/Facebook.png"> Our Facebook Page</a>
