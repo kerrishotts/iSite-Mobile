bd841ecef4f92305519a5e08c9838264eea28a14
Kerri Shotts <kerrishotts@gmail.com>
2011-05-26 00:00:25 -0500

    Split menu list out in order to simply index.php page

---
+++ menu.php
@@ -0,0 +1,11 @@
+                    <img src="./images/church326.jpg" width=100% />
+                    <h2>Navigation</h2>
+                    <div class="navSubItemGroup" id="grpMainMenu" >
+                        <a id="mnuMainHome"       title="Home"       href="javascript:loadContent('./home.php', updateMainMenu );"       class="navSubItem sel"><img src="./images/tabsfreecolor/Circle-Info.png"> Home</a>
+                        <a id="mnuMainNews"       title="News"       href="javascript:loadContent('./news.php', updateMainMenu );"       class="navSubItem"><img src="./images/tabsfreecolor/RSS-Alternative.png"> News &amp; Events</a>
+                        <a id="mnuMainMinistries" title="Ministries" href="javascript:loadContent('./ministries.php', updateMainMenu );" class="navSubItem"><img src="./images/tabsfreecolor/Heart.png"> Ministries</a>
+                        <a id="mnuMainPodcasts"   title="Podcasts"   href="javascript:loadContent('./podcasts.php', updateMainMenu );"   class="navSubItem"><img src="./images/tabsfreecolor/Music.png"> Podcasts</a>
+                        <a id="mnuMainTweets"     title="Tweets"     href="javascript:loadContent('./tweets.php', updateMainMenu );"     class="navSubItem"><img src="./images/tabsfreecolor/Twitter.png"> Latest Tweets</a>
+                        <a id="mnuMainFacebook"   title="Facebook"   href="javascript:loadContent('./facebook.php', updateMainMenu );"   class="navSubItem"><img src="./images/tabsfreecolor/Facebook.png"> Our Facebook Page</a>
+                        <a id="mnuMainContact"    title="Contact"    href="javascript:loadContent('./contact.php', updateMainMenu );"    class="navSubItem"><img src="./images/tabsfreecolor/Chat-Bubble.png"> Contact Us</a>
+                    </div>
