e7ea281b0026e296166f6175ee40fb019258e2c5
Kerri Shotts <kerrishotts@gmail.com>
2011-05-26 17:11:50 -0500

    New example page to contain examples (coming)

---
+++ examples.php
@@ -0,0 +1,16 @@
+<div class="content">
+    <h1>iSite Mobile Demo Examples</h1>
+    <p>Examples...</p>
+    <div class="listGroup">
+        <a class="listItem arrow" href="javascript:loadContent('./worship.php', updateMainMenu,'slideOut', './ministries.php' );">Worship <span class="ipad minor floatRight">Sundays at 10am and 6pm</span></a>
+        <a class="listItem arrow" href="javascript:loadContent('./ministries/sundayschool.php', updateMainMenu,'slideOut', './ministries.php' );">Sunday School <span class="ipad minor floatRight">Wednesdays at 7pm</span></a>
+    </div>
+
+    <div class="listGroup">
+        <a class="listItem arrow" href="javascript:loadContent('./ministries/smallgroup1.php', updateMainMenu,'slideOut' , './ministries.php');">Prime Time <span class="ipad minor floatRight">Saturday Evenings</span></a>
+        <a class="listItem arrow" href="javascript:loadContent('./ministries/smallgroup2.php', updateMainMenu,'slideOut' , './ministries.php');">Adult Bible Study <span class="ipad minor floatRight">Monday Evenings</span></a>
+        <a class="listItem arrow" href="javascript:loadContent('./ministries/smallgroup2.php', updateMainMenu,'slideOut' , './ministries.php');">Women's Prayer <span class="ipad minor floatRight">Monday and Thursday Evenings</span></a>
+        <a class="listItem arrow" href="javascript:loadContent('./ministries/smallgroup2.php', updateMainMenu,'slideOut' , './ministries.php');">Men's Prayer <span class="ipad minor floatRight">Saturday Mornings</span></a>
+    </div>
+
+</div>
