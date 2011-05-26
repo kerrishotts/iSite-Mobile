b5d24a370b19e423366a5308cd379e94b2c48a20
Kerri Shotts <kerrishotts@gmail.com>
2011-05-26 00:00:43 -0500

    Updated to support dynamic loading; split menu items out to menu.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <title>
-            Godfrey Church of the Nazarene (Mobile)
+            iSite Mobile Demo
         </title>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="viewport" content="width=device-width, maximum-scale=1.0" />
@@ -27,22 +27,12 @@
     </head>
     <body class="ipad landscape" id="pnlBody" ontouchmove="BlockMove(event);">
         <div class="panel" id="menuPanel">
-            <div class="navBar">
-                My Mobile Church
+            <div class="navBar" id="navMainArea">
+                <span id="navMenuTitle">Menu</span>
             </div>
             <div class="container" id="pnlMainArea">
                 <div class="content" id="menuMain">
-                    <img src="./images/church326.jpg" width=100% />
-                    <h2>Navigation</h2>
-                    <div class="navSubItemGroup" id="grpMainMenu" >
-                        <a id="mnuMainHome"       title="Home"       href="javascript:loadContent('./home.php', updateMainMenu );"       class="navSubItem sel"><img src="./images/tabsfreecolor/Circle-Info.png"> Home</a>
-                        <a id="mnuMainNews"       title="News"       href="javascript:loadContent('./news.php', updateMainMenu );"       class="navSubItem"><img src="./images/tabsfreecolor/RSS-Alternative.png"> News &amp; Events</a>
-                        <a id="mnuMainMinistries" title="Ministries" href="javascript:loadContent('./ministries.php', updateMainMenu );" class="navSubItem"><img src="./images/tabsfreecolor/Heart.png"> Ministries</a>
-                        <a id="mnuMainPodcasts"   title="Podcasts"   href="javascript:loadContent('./podcasts.php', updateMainMenu );"   class="navSubItem"><img src="./images/tabsfreecolor/Music.png"> Podcasts</a>
-                        <a id="mnuMainTweets"     title="Tweets"     href="javascript:loadContent('./tweets.php', updateMainMenu );"     class="navSubItem"><img src="./images/tabsfreecolor/Twitter.png"> Latest Tweets</a>
-                        <a id="mnuMainFacebook"   title="Facebook"   href="javascript:loadContent('./facebook.php', updateMainMenu );"   class="navSubItem"><img src="./images/tabsfreecolor/Facebook.png"> Our Facebook Page</a>
-                        <a id="mnuMainContact"    title="Contact"    href="javascript:loadContent('./contact.php', updateMainMenu );"    class="navSubItem"><img src="./images/tabsfreecolor/Chat-Bubble.png"> Contact Us</a>
-                    </div>
+                    <? include ('./menu.php'); ?>
                 </div>
             </div>
         </div>
@@ -50,10 +40,10 @@
             <div class="navBar" id="navBodyArea">
                 <a id="btnBack" href="javascript:loadContent (returnTo.pop(), updateMainMenu, 'slideBack')" class="button back black"><span></span>Back</a>
                 <a id="btnMenu" href="javascript:toggleMenu ()" class="button black">Menu</a>
-                <span id="navSiteTitle">Sample Church of the Nazarene &bull; </span><span id="navBodyTitle">Home</span>
+                <span id="navSiteTitle">Sample Church of the Nazarene</span><span id="navSiteBullet"> &bull; </span><span id="navBodyTitle">Home</span>
             </div>
             <div class="container"  id="pnlBodyArea">
-                <? include ('./home.php'); ?>
+                <!-- this to be replaced by content at startup -->
             </div>
         </div>
         <div id="loader">
@@ -63,7 +53,14 @@
             </div>
         </div>
         <script>
-            startApp( ); // tell our app to start and load the Podcasts Page.
+        
+            // startup variables
+            var mySiteName = "Sample Church of the Nazarene";
+            var myMenuName = "My Mobile Church";
+            var myStartPage = "./home.php";
+            var myStartName = "Home";
+        
+            startApp( ); // tell our app to start
         </script>
     </body>
 </html>
