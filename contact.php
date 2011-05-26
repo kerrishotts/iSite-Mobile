229906106a658d001f7a9e402491afc51d264695
Kerri Shotts <kerrishotts@gmail.com>
2011-05-26 17:11:37 -0500

    Updated to reflect demo status

--- contact.php
+++ contact.php
@@ -1,9 +1,10 @@
 <div class="content">
-    <h1>Send Us a Message</h1>
-    <p>We'd love to hear from you! You can send us a method using the form below, or via several different social media applications.</p>
+    <h1>Send a Message</h1>
+    <p>The following demonstrates the ability to launch other iDevice applications via links as well as the ability to hide certain items based upon iDevice.</p>
     <div class="listGroup">
-        <a class="listItem arrow" href="twitter://user?screen_name=godfreynaz">Tweet us @godfreynaz <span class="ipad floatRight minor">Chirp, Chirp!</span></a>
-        <a class="listItem arrow" href="mailto:webmaster@godfreynazarene.org?subject=Question">Send e-mail to us <span class="ipad floatRight minor">Remember, Nothing Confidential!</span></a>
-        <a class="listItem arrow" href="tel:18005555555">Call us at 1 (800) 555-5555<span class="ipad minor floatRight">So 20th century, dude!</span></a>
+        <a class="listItem arrow" href="twitter://user?screen_name=photokandy">Tweet me @photokandy <span class="ipad floatRight minor">Chirp, Chirp!</span></a>
+        <a class="listItem arrow" href="mailto:info@photokandy.com?subject=Question">Send e-mail to me <span class="ipad floatRight minor">Remember, Nothing Confidential!</span></a>
+        <a class="listItem arrow iphoneOnly" href="tel:18005555555">Call me at 1 (800) 555-5555 (so not a real phone number!)<span class="ipad minor floatRight">So 20th century, dude!</span></a>
+        <a class="listItem ipadOnly">Call me at 1 (800) 555-5555 (so not a real phone number!)<span class="ipad minor floatRight">Sorry, no dialing from an iPad!</span></a>
     </div>    
 </div>
