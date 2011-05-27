1d5bfd791b23e9f484bbb337b8bfacde64ee3551
Kerri Shotts <kerrishotts@gmail.com>
2011-05-26 21:52:26 -0500

    News. Updated 5/26/2011

--- news.php
+++ news.php
@@ -1,3 +1,23 @@
 <div class="content">
-    <p>News</p>
+    <h1>Project News</h1>
+    <p>Don't forget to check out the <a href="https://github.com/kerrishotts/iSite-Mobile">project on GitHub</a> to download the
+       framework and see the most recent, nail-bitingly gritty happenings.</p>
+    <h2>May 26th, 2011 - Conversion to Demo Site</h2>
+    <ul>
+        <li>Converted the content to a demo site.</li>
+        <li>Fixed some CSS with listGroups and regular lists.</li>
+        <li>Added examples.</li>
+        <li>Updated this page.</li>
+    </ul>
+    <h2>May 25th, 2011 - Abstraction</h2>
+    <ul>
+        <li>Abstracted the titles in index.php out to JavaScript.</li>
+        <li>Updated to load the initial page view dynamically.</li>
+        <li>Split menu out via PHP. TODO: make it load dynamically as well.</li>
+    </ul>
+    <h2>May 24th, 2011 - Git</h2>
+    <ul>
+        <li>Added to GitHub.</li>
+    </ul>
+
 </div>
