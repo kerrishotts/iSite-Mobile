728c5b19310cb9a25044f9e66d77eece913e5102
Kerri Shotts <kerrishotts@gmail.com>
2011-05-26 21:52:03 -0500

    Updated to include links to examples.

--- examples.php
+++ examples.php
@@ -1,16 +1,15 @@
 <div class="content">
     <h1>iSite Mobile Demo Examples</h1>
-    <p>Examples...</p>
+    <p>The following pages show examples of the various parts of the framework.</p>
+    
     <div class="listGroup">
-        <a class="listItem arrow" href="javascript:loadContent('./worship.php', updateMainMenu,'slideOut', './ministries.php' );">Worship <span class="ipad minor floatRight">Sundays at 10am and 6pm</span></a>
-        <a class="listItem arrow" href="javascript:loadContent('./ministries/sundayschool.php', updateMainMenu,'slideOut', './ministries.php' );">Sunday School <span class="ipad minor floatRight">Wednesdays at 7pm</span></a>
+        <h2>Lists</h2>
+        <a class="listItem arrow" href="javascript:loadContent('./examples/list1.php', updateMainMenu,'slideOut', './examples.php' );">Standard Lists <span class="ipad minor floatRight">Just straight-up HTML</span></a>
+        <a class="listItem arrow" href="javascript:loadContent('./examples/list2.php', updateMainMenu,'slideOut', './examples.php' );">iTunes Lists <span class="ipad minor floatRight">Let's Listen to Some Music!</span></a>
+        <h2>Widgets</h2>
+        <a class="listItem arrow" href="javascript:loadContent('./examples/buttons.php', updateMainMenu,'slideOut' , './examples.php');">Buttons <span class="ipad minor floatRight">Look at those pretty colors!</span></a>
+        <a class="listItem arrow" href="javascript:loadContent('./examples/textentry.php', updateMainMenu,'slideOut' , './examples.php');">Text Entry <span class="ipad minor floatRight">Get out you' virtual keyboard, yo!</span></a>
     </div>
 
-    <div class="listGroup">
-        <a class="listItem arrow" href="javascript:loadContent('./ministries/smallgroup1.php', updateMainMenu,'slideOut' , './ministries.php');">Prime Time <span class="ipad minor floatRight">Saturday Evenings</span></a>
-        <a class="listItem arrow" href="javascript:loadContent('./ministries/smallgroup2.php', updateMainMenu,'slideOut' , './ministries.php');">Adult Bible Study <span class="ipad minor floatRight">Monday Evenings</span></a>
-        <a class="listItem arrow" href="javascript:loadContent('./ministries/smallgroup2.php', updateMainMenu,'slideOut' , './ministries.php');">Women's Prayer <span class="ipad minor floatRight">Monday and Thursday Evenings</span></a>
-        <a class="listItem arrow" href="javascript:loadContent('./ministries/smallgroup2.php', updateMainMenu,'slideOut' , './ministries.php');">Men's Prayer <span class="ipad minor floatRight">Saturday Mornings</span></a>
-    </div>
 
 </div>
