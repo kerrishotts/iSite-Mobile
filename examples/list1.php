bd627330c9b7a99c142c040fd8d298b38c74c435
Kerri Shotts <kerrishotts@gmail.com>
2011-05-26 21:52:17 -0500

    New: List Examples using UL/LI and DIV/As.

---
+++ examples/list1.php
@@ -0,0 +1,40 @@
+<script type="text/javascript">
+    setPageTitle ("Standard Lists");
+</script>
+<div class="content">
+    <h1>Standard Lists</h1>
+    <p>You can create standard lists using the UL and LI items or any other item as long as the parent is classed "listGroup" and the children items are classed "listItem". H2s that are embedded will
+       gain the normal "header" look-and-feel present in iOS.</p>
+    
+    <h3>Example 1:</h3>
+    <p>This example uses only ULs and LIs. The header is a LI classed "listHeader".</p>
+    <ul class="listGroup">
+        <li class="listItem">List Item 1</li>
+        <li class="listItem">List Item 2</li>
+        <li class="listItem">List Item 3</li>
+        <li class="listItem">List Item 4</li>
+        <li class="listItem">List Item 5</li>
+        <li class="listHeader">List Header</li>
+        <li class="listItem">List Item 6</li>
+        <li class="listItem">List Item 7</li>
+        <li class="listItem">List Item 8</li>
+        <li class="listItem">List Item 9</li>
+        <li class="listItem">List Item 10</li>
+    </ul>
+    
+    <h3>Example 2:</h3>
+    <p>This example uses only DIVs and Anchors. The header is an H2.</p>
+    <div class="listGroup">
+        <h2>Television News Sources</h2>
+        <a class="listItem" href="http://www.cnn.com" target="_blank">CNN</a>
+        <a class="listItem" href="http://www.nbc.com" target="_blank">NBC</a>
+        <a class="listItem" href="http://www.cbs.com" target="_blank">CBS</a>
+        <a class="listItem" href="http://www.abc.com" target="_blank">ABC</a>
+        <h2>Newspaper Sources</h2>
+        <a class="listItem" href="http://www.cnn.com" target="_blank">CNN</a>
+        <a class="listItem" href="http://www.nbc.com" target="_blank">NBC</a>
+        <a class="listItem" href="http://www.cbs.com" target="_blank">CBS</a>
+        <a class="listItem" href="http://www.abc.com" target="_blank">ABC</a>
+    </div>
+
+</div>
